d70f5b1d89dc5b64b06d93973fc77131097cc116
Arastoo Setayeshzadeh <159822772+arasto0@users.noreply.github.com>
2024-02-19 21:59:12 +0330

    Add files via upload

---
+++ homework2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    printf("ASCII values for the characters in the name Arastoo Setayeshzadeh:\n");
+    printf("------------------------------\n");
+    printf("|    A      |     %d       |", 'A');
+    printf("|    r      |     %d       |", 'r');
+    printf("|    a      |     %d       |", 'a');
+    printf("|    s      |     %d       |", 's');
+    printf("|    t      |     %d       |", 't');
+    printf("|    o      |     %d       |", 'o');
+    printf("|    o      |     %d       |", 'o');
+    printf("|            |              |\n");
+    printf("|    S      |     %d       |", 'S');
+    printf("|    e      |     %d       |", 'e');
+    printf("|    t      |     %d       |", 't');
+    printf("|    a      |     %d       |", 'a');
+    printf("|    y      |     %d       |", 'y');
+    printf("|    e      |     %d       |", 'e');
+    printf("|    s      |     %d       |", 's');
+    printf("|    h      |     %d       |", 'h');
+    printf("|    z      |     %d       |", 'z');
+    printf("|    a      |     %d       |", 'a');
+    printf("|    d      |     %d       |", 'd');
+    printf("|    e      |     %d       |", 'e');
+    printf("|    h      |     %d       |", 'h');
+    
+    return 0;
+}
